66b63d1d3a445a38e1cc6718cfc3f6368ad75176
HardikThakkar94 <hardik.thakkar00@gmail.com>
2021-04-22 16:17:57 -0400

    :pencil2: :hammer: :wrench: :lipstick:
    --replaced "sign in" with "login". --

--- lib/src/screens/login.dart
+++ lib/src/screens/login.dart
@@ -110,7 +110,7 @@ class _LoginScreenState extends State<LoginScreen> {
                   color: Colors.black, //white or black
                 ),
                 label: Text(
-                  'Sign in with Google',
+                  'Login with Google',
                   style: TextStyle(
                       fontSize: 20, color: Colors.black), //white or black
                 ),
@@ -132,7 +132,7 @@ class _LoginScreenState extends State<LoginScreen> {
                   color: Colors.black, //white or black
                 ),
                 label: Text(
-                  'Sign in with Phone',
+                  'Login with Phone',
                   style: TextStyle(
                       fontSize: 20, color: Colors.black), //white or black
                 ),
